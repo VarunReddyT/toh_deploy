7012410c24a4b7ed7f32587f2b5bac300f668bd5
Varun-MSI <tvarun2014@gmail.com>
2024-04-07 20:57:31 +0530

    New

--- backend/index.js
+++ backend/index.js
@@ -2,16 +2,13 @@ const express = require('express');
 const router = express.Router();
 const cors = require('cors');
 const mongoose = require('mongoose');
-const guestDetails = require('./routes/guestDetails.js');
-const guestUpload = require('./routes/guestUpload.js'); 
-const tollCheckRecords = require('./routes/tollCheckRecords.js');
-const tollLogin = require('./routes/tollLogin.js');
-const tollLogout = require('./routes/tollLogout.js');
-const tollUpload = require('./routes/tollUpload.js');
-const tollChRcImages = require('./routes/tollChRcImages.js');
-const statistics = require('./routes/statistics.js')
-const feedback = require('./routes/feedback.js');
 const app = express();
+const multer = require('multer');
+const TollPlaza = require('../models/TollPlazaSch');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 require('dotenv').config();
 
 // ^ defining port
@@ -40,15 +37,66 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-router.get('/guestDet' ,guestDetails);
-router.post('/guestUp', guestUpload);
-router.get('/checkRecords', tollCheckRecords);
-router.post('/login', tollLogin);
-router.get('/logout', tollLogout);
-router.post('/tollupload', tollUpload);
-router.get('/getIm', tollChRcImages);
-router.get('/stats', statistics);
-router.post('/feedback',feedback);
+// router.get('/guestDet' ,guestDetails);
+// router.post('/guestUp', guestUpload);
+// router.get('/checkRecords', tollCheckRecords);
+// router.post('/login', tollLogin);
+// router.get('/logout', tollLogout);
+// router.post('/tollupload', tollUpload);
+// router.get('/getIm', tollChRcImages);
+// router.get('/stats', statistics);
+// router.post('/feedback',feedback);
+
+
+app.use(cookieParser());
+
+// & Multer config for TollUpload
+const TollUp = multer.memoryStorage();
+const Tollupload = multer({ storage: TollUp, limits: { fieldSize: 25 * 1024 * 1024 } })
+
+// & JWT
+const createToken = (id) => {
+    return jwt.sign({ id }, 'TiresOnHighway', { expiresIn: 60 * 60 * 1000 });}
+
+// ! Login Route
+app.post('/login', Tollupload.any(), async (req, res) => {
+    const { toll, password } = req.body;
+    try {
+      const user = await TollPlaza.findOne({ username: toll });
+      console.log(user);
+      if (user) {
+        try {
+          const passMatch = await bcrypt.compare(password, user.password);
+          if (passMatch) {
+            console.log("Password Matched");
+            try {
+              const token = createToken(user._id);
+              // console.log(token);
+              res.cookie('tollLogin', token, {  maxAge: 60 * 60 * 1000 });
+              // sameSite: 'None'  -> for CORS purposes and controlling the cookie to be sent only to the same origin
+              // secure : true -> is not recommended for development purposes as we can't access a cookie using document.cookie in the client side 
+              // path: '/' -> to make the cookie available to all the routes
+              // domain: `http://${req.hostname}:3000`} -> to make the cookie available to all the subdomains
+              console.log("Success");
+              res.send("Success");
+            } catch (err) {
+              console.log(err);
+            }
+          }
+          else {
+            console.log("Not Allowed");
+            res.send("Not Allowed");
+          }
+        }
+        catch (err) {
+          console.log(err);
+        }
+      }
+    }
+    catch (err) {
+      console.log(err);
+    }
+  });
 
 // ^ Server listening on port 4000
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
